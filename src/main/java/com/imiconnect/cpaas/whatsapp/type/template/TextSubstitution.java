faceb888c4a73cb6819b7358fbb4e71803b392fd
Nicholas Kehagias <kehagias@cisco.com>
2021-10-23 22:23:36 -0400

    Add whatsapp template substitution types

---
+++ src/main/java/com/imiconnect/cpaas/whatsapp/type/template/TextSubstitution.java
@@ -0,0 +1,24 @@
+package com.imiconnect.cpaas.whatsapp.type.template;
+
+import com.imiconnect.cpaas.core.type.StringWrapper;
+import lombok.Value;
+
+import static java.util.Objects.requireNonNull;
+
+/** Whatsapp template content defining a plain text substitution. */
+@Value
+public final class TextSubstitution implements Substitution, StringWrapper {
+
+  private final Type contentType = Type.TEXT;
+  private final String text;
+
+  public TextSubstitution(String text) {
+    this.text = requireNonNull(text, "text value for substitution can not be null.");
+  }
+
+  @Override
+  public String get() {
+    return this.text;
+  }
+
+}
---
+++ src/main/java/com/imiconnect/cpaas/whatsapp/type/template/UrlSubstitution.java
@@ -0,0 +1,22 @@
+package com.imiconnect.cpaas.whatsapp.type.template;
+
+import lombok.Value;
+
+import static java.util.Objects.requireNonNull;
+
+/** Whatsapp template content that defines a URL substitution. */
+@Value
+public final class UrlSubstitution implements Substitution {
+
+  private final Type contentType = Type.URL;
+
+  /**
+   * This will be appended to URLs that are configured within the Connect platform GUI.
+   */
+  private final String suffix;
+
+  public UrlSubstitution(String suffix) {
+    this.suffix = requireNonNull(suffix, "url suffix can not be null.");
+  }
+
+}
